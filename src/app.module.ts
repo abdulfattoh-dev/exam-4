b55c1f00aa0703ee664851f0623a706db36b4c39
abdulfattoh-dev <abdulfattoh.dev@gmail.com>
2025-05-27 21:40:51 +0500

    all moduls added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import config from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: config.PG_HOST,
+      port: config.PG_PORT,
+      username: config.PG_USER,
+      password: config.PG_PASS,
+      database: config.PG_DB,
+      synchronize: true,
+      logging: false,
+      autoLoadModels: true,
+      models: []
+    }),
+    UsersModule,
+    ProductsModule,
+    CategoriesModule,
+    ReviewsModule]
 })
-export class AppModule {}
+export class AppModule { }

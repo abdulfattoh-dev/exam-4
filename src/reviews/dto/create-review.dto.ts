6a00a6c0c120710162745c799e26229ad234ebff
Kiyomiddinova-Farida <farida.qiyomiddinova.09@gmail.com>
2025-05-29 20:27:37 +0500

    review create.review.dto added

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,18 @@
-export class CreateReviewDto {}
+import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
+
+export class CreateReviewDto {
+  @IsInt()
+  user_id: number;
+
+  @IsInt()
+  product_id: number;
+
+  @IsString()
+  @IsOptional()
+  comment?: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating: number;
+}

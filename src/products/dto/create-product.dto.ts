3b2477f8873cacd66c35c18404d22d30fff21128
abdulfattoh-dev <abdulfattoh.dev@gmail.com>
2025-05-31 21:07:47 +0500

    errors fixed

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, IsDecimal, IsNumber } from "class-validator";
 export class CreateProductDto {
     @IsNotEmpty()
     @IsString()
-    name: string
+    name: string;
 
     @IsString()
     description: string;
